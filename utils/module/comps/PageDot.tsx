d9a372f039f220f4376cdf42bc3cd2c6511bc053
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-06 20:55:26 +0100

    changed size and color of dots

--- utils/module/comps/PageDot.tsx
+++ utils/module/comps/PageDot.tsx
@@ -14,18 +14,20 @@ function PageDot({
   index,
   setCurrentPage,
 }: Props): ReactElement {
-  const size = current ? 20 : 15;
+  const size = current ? 15 : 10;
 
   return (
     <motion.div
       style={{
         width: `${size}px`,
         height: `${size}px`,
-        opacity: 0.5,
+        opacity: 0.3,
         backgroundColor: "gray",
         borderRadius: "50%",
         margin: "5px 0",
+        cursor: "pointer",
       }}
+      whileHover={{ scale: 1.2 }}
       onClick={() => {
         setCurrentPage(index);
       }}
