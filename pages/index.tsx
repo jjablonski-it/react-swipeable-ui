43a0acc979424fe01b605df827efa1962fc64146
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-13 23:01:23 +0100

    tweaked css for navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Pages.module.css";
 
 function Home() {
   return (
-    <Scroll navigation={false} pageIndicator="hover">
+    <Scroll navigation={true} pageIndicator="always">
       <Page text="One" className={styles.one} pageName="About me" />
       <Page text="Two" className={styles.two} pageName="Skills" />
       <Page text="Three" className={styles.three} pageName="Projects" />
