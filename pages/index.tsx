b5dd127c826da4855ea2c8c6727dd89603dc9882
Jakub Jabłoński <jjablonski.it@gmail.com>
2021-01-13 19:40:20 +0100

    removed pages test duplicate

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,6 @@ import Scroll from "../utils/module/Scroll";
 import styles from "../styles/Pages.module.css";
 
 function Home() {
-  const test = (
-    <>
-      <Page text="One" className={styles.one} />
-      <Page text="Two" className={styles.two} />
-      <Page text="Three" className={styles.three} />
-      <Page text="Four" className={styles.four} />
-    </>
-  );
   return (
     <Scroll>
       <Page text="One" className={styles.one} pageName="About me" />
